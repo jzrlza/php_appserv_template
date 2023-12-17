8cd51de8868346d252e514bcd54c5c03c5660ff6
jzrlza <parinvut12345@gmail.com>
2023-12-17 15:07:17 +0700

    .

--- test/index.php
+++ test/index.php
@@ -1,6 +1,8 @@
 <?php
 
-$shellPY = "C:\Users\JBCOM\AppData\Local\Programs\Python\Python311\python";
+$config = parse_ini_file( __DIR__ .'\config.ini', true);
+
+$shellPY = $config['python']['path'];
 $phpScriptPath = __DIR__ . '\index.php';  // Adjust the path accordingly
 $pythonScriptPath = __DIR__ . '\pytest.py';  // Adjust the path accordingly
 //$pythonScript = 'pytest.py';
@@ -8,6 +10,6 @@ $pythonScriptPath = __DIR__ . '\pytest.py';  // Adjust the path accordingly
 // Use exec to run the Python script and capture its output
 exec($shellPY . " " . $pythonScriptPath, $output);
 
-echo $pythonScriptPath . " <br> " . $output[0];
+echo $shellPY . " <br> " . $output[0];
 
 ?>
