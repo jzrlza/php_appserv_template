088c66943b2aedbff1d68e2387072d60f2c01be0
jzrlza <parinvut12345@gmail.com>
2023-12-17 14:26:12 +0700

    this is weird

--- test/index.php
+++ test/index.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "hello world";
+$shellPY = "C:\Users\JBCOM\AppData\Local\Programs\Python\Python311\python";
+$phpScriptPath = __DIR__ . '\index.php';  // Adjust the path accordingly
+$pythonScriptPath = __DIR__ . '\pytest.py';  // Adjust the path accordingly
+//$pythonScript = 'pytest.py';
+
+// Use exec to run the Python script and capture its output
+exec($shellPY . " " . $pythonScriptPath, $output);
+
+echo $pythonScriptPath . " <br> " . $output[0];
 
 ?>
---
+++ test/pytest.py
@@ -0,0 +1,2 @@
+import os, sys, json
+print(json.dumps(["a"]))
